111a026a4006749646363b585c9333eff6a1dccd
Galvan <72113166@continental.edupe>
2024-05-20 16:32:11 -0500

    vistas

--- public/controladores/empresa.js
+++ public/controladores/empresa.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     
     $('#btn_modal').on("click", function() {
-        $('#modal_Examen').modal("show")
+        $('#modal_empresa').modal("show")
+    })
+
+    $('#btnGuardar').on("click", async function() {   
+        console.log('guardar')
     })
 
 })
